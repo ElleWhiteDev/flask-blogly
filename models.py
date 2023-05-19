01bb841ff5af2c8d1800cfb12de5b757d281f321
Elizabeth White <redfragle88@gmail.com>
2023-05-19 12:03:26 -0400

    finished base template

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
 """Models for Blogly."""
+
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = (db.Text, nullable=False)
+    last_name = (db.Text, nullable=False)
+    img_url = (db.Text, default="https://play-lh.googleusercontent.com/W1Rz_g5pu2i07INW9uqiD-Tj30e_F8HppnuE41WFfXWlKXzPDCL6_B52sJ2sCONkrA=w240-h480-rw")
