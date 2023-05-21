222653c989d797582bcbed8cdd7effbe7acbcafe
ewhite <ellewhitedev@gmail.com>
2023-05-21 09:23:43 -0400

    page and route outlines added

--- app.py
+++ app.py
@@ -14,3 +14,32 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+
+@app.route("/")
+def home_page():
+    return redirect("/users")
+
+@app.route("/users")
+def list_users():
+    return render_template("users.html")
+
+@app.route("/users/new", methods=["POST"])
+def add_user():
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    img_url = request.form["img-url"]
+
+    return render_template("new-user-form.html", first-name=first_name, last-name=last_name, img-url=img_url)
+
+@app.route("/users/<user.id>")
+def user_details():
+    return render_template("user-detail.html")
+
+@app.route("/users/<user.id>/edit", methods=["POST"])
+def edit_user():
+    return render_template("user-detail.html")
+
+@app.route("/users/<user.id>/delete", methods=["POST"])
+def delete_user():
+    return redirect("/users")
